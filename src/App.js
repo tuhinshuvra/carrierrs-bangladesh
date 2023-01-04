bbd5db365e783c073f2cfa57c868a623311486c9
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2023-01-04 11:09:52 +0600

    added footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './Pages/Router/Router/Router';
@@ -7,10 +6,6 @@ function App() {
   return (
     <div className="App">
       <RouterProvider router={router}></RouterProvider>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
     </div>
   );
 }
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
-        <div>
+        <div className=' w-[1200px] mx-auto'>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
