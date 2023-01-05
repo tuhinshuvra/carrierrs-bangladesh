23849222e449498d3a525c6210f2b4647becb734
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2023-01-05 17:29:01 +0600

    added signin and register page

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Register = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [signUpError, setSignUpError] = useState('');
+    const [createdUserEmail, setCreatedUserEmail] = useState('');
+
+
+    const submitHandler = (data) => {
+        console.log(data);
+    }
+
+
+
     return (
-        <div>
-            <h2>This is Register Section</h2>
+        <div className=' flex justify-center items-center '>
+            <div className=' w-96 p-4'>
+                <h2 className=' text-xl font-bold  text-center'>Register</h2>
+                <form onSubmit={handleSubmit(submitHandler)}>
+                    {signUpError && <p className=' text-red-600'>{signUpError}</p>}
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text">Name</span> </label>
+                        <input type="name"
+                            name='name'
+                            {...register("name", { required: true })}
+                            placeholder="Name"
+                            className="input input-bordered w-full"
+                        />
+                        {errors.name && <p className='text-red-600'>Name is required</p>}
+                    </div>
+
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text">Email</span> </label>
+                        <input type="email"
+                            name='email'
+                            {...register("email", { required: true })}
+                            placeholder="Email"
+                            className="input input-bordered w-full"
+                        />
+                        {errors.email && <p className='text-red-600'>Email is required</p>}
+                    </div>
+
+                    <div className="form-control w-full">
+                        <label className="label"><span className="label-text">Password</span> </label>
+                        <input
+                            type="password"
+                            name='password'
+                            {...register("password",
+                                {
+                                    required: true,
+                                    minLength: { value: 6, message: "Password length must be minimum 6" },
+                                    pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: 'Password must have uppercase, number and special characters' }
+                                },
+                            )}
+
+                            placeholder="Password"
+                            className="input input-bordered w-full"
+                        />
+                        {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
+                    </div>
+
+                    <input className=' mt-3 btn btn-primary form-control w-full' type="submit" value='SingUp' />
+                    <p className=' my-3 text-center'> <b> Already have an account?</b> <Link to='/signin'> <span className=' text-accent '>Go to login</span></Link> </p>
+                </form>
+            </div>
         </div>
     );
 };
--- src/Pages/Signin/Signin.js
+++ src/Pages/Signin/Signin.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { useContext } from 'react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const Signin = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const [loginError, setLoginError] = useState('');
+    const [loginUserEmail, setLoginUserEmail] = useState('');
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const from = location.state?.form?.pathname || '/';
+
+
+    const handleLogin = (data) => {
+        console.log(data);
+
+    }
+
     return (
-        <div>
-            <h2>This is Signin Page</h2>
+        <div className=' h-[500px] flex justify-center items-center'>
+            <div className=' '>
+                <h2 className=' text-3xl text-center'>Singin</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+                    <div> {loginError && <p className=' text-red-600'>{loginError}</p>}</div>
+                    <label className="label"><span className="label-text font-bold">Email</span></label>
+                    <input type="email"  {...register("email",
+                        { required: 'Email is required' })}
+                        className="input input-bordered w-full" placeholder="Email" />
+                    {errors.email && <p className=' text-red-600' role="alert">{errors.email?.message}</p>}
+
+                    <label className="label"><span className="label-text font-bold">Password</span></label>
+                    <input type="password"  {...register("password", { required: 'Password is required' })}
+                        className="input input-bordered  w-full" placeholder="Password" />
+                    {errors.password && <p className=' text-red-600' role="alert">{errors.password?.message}</p>}
+
+                    <label className="label"><span className="label-text">Forget Password ? </span></label>
+
+                    <input className=' btn btn-sm  btn-primary my-3 float-right w-full' value='Login' type="submit" />
+                </form>
+                <p>New to doctors portal<Link to='/register' className=' text-accent '> Create New Account</Link> </p>
+                <div className="divider">OR</div>
+                <input className='btn btn-sm btn-primary btn-outline my-3 float-right w-full' value='Continue with Google' type="submit" />
+            </div>
         </div>
     );
 };
