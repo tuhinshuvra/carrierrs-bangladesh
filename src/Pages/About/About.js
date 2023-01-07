84f0f0d2220b66616c5266493cee43e4c52ef42c
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2023-01-07 10:26:19 +0600

    updated about and contact form

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>This is about page</h2>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <h1 className="text-5xl font-bold">About Carriers Bangladesh</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button className="btn btn-primary">More Details</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -3,7 +3,51 @@ import React from 'react';
 const Contact = () => {
     return (
         <div>
-            <h2>This is Contact Page</h2>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 w1/2">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" placeholder="Full Name" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Phone</span>
+                                </label>
+                                <input type="text" placeholder="phone" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Message</span>
+                                </label>
+                                <input type="textarea" rows="4" cols="50" className="input input-bordered" />
+                            </div>
+
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="text-center lg:text-left w-1/2">
+                        <h1 className="text-3xl font-bold mb-5">Get in Touch!</h1>
+                        <p>Address: H-20, Shah Makdum Avenue, Uttara-11, Dhaka</p>
+                        <p>Email: abedefth@gmail.com</p>
+                        <p>WhatsApp +8801123456789</p>
+                        <p>Linkedin: @abcdefgh</p>
+                        <p>Facebook @abcdefghijk</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
