bfc4e569981f7f75a87ece828fc8bac09104e323
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2023-01-08 18:45:27 +0600

    updated profile page

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,64 +1,137 @@
+// import React from 'react';
+
+// const Profile = () => {
+//     return (
+//         <div className=''>
+
+//             <div className=" columns-4 flex justify-end ">
+//                 <img className="rounded-circle" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" alt='' />
+//                 <span className=" font-bold">Email : </span><span className="text-black-50">banglibabu@gmail.com</span>
+//             </div>
+
+//             <h2 className='text-center font-bold text-4xl my-10'>Profile Settings</h2>
+
+//             <div className=' columns-8 mx-auto my-6'>
+//                 <label className="label"> <span className="label-text">Name</span>  </label>
+//                 <input type="text" className="form-control input input-bordered" placeholder="Full Name" value="" />
+//                 <label className="label"> <span className="label-text">Surname</span>  </label>
+//                 <input type="text" className="form-control input input-bordered" placeholder="surname" value="" />
+//             </div>
+
+//             <div className=" columns-8 mx-auto my-6">
+//                 <label className="labels">Phone/Mobile</label>
+//                 <input type="text" className="form-control input input-bordered" placeholder="enter phone/mobile" value="" />
+//                 <label className="labels">Address Line 1</label>
+//                 <input type="text" className="form-control input input-bordered" placeholder="enter address line 1" value="" />
+//                 <label className="labels">Address Line 2</label>
+
+//             </div>
+
+//             <div className=" columns-8 mx-auto my-6">
+//                 <label className="labels">Postcode</label>
+//                 <input type="text" className="form-control input input-bordered" placeholder="enter address line 2" value="" />
+//                 <label className="labels">State</label>
+//                 <input type="text" className="form-control input input-bordered" placeholder="enter address line 2" value="" />
+//             </div>
+
+//             <div className=" columns-8 mx-auto my-6">
+//                 <label className="labels">Area</label>
+//                 <input type="text" className="form-control input input-bordered" placeholder="enter address line 2" value="" />
+//                 <label className="labels">Country</label>
+//                 <input type="text" className="form-control input input-bordered" placeholder="country" value="" />
+//             </div>
+
+
+//             <div className=" columns-8 my-6">
+//                 <label className="labels">Education</label>
+//                 <input type="text" className="form-control input input-bordered" placeholder="education" value="" />
+//             </div>
+
+
+//             <div className="columns-8">
+//                 <label>Edit Experience</label>
+//                 <input type="text" className="form-control input input-bordered" placeholder="experience" value="" />
+//             </div>
+
+//             <div className=" columns-8 my-6">
+//                 <label className="labels">Additional Details</label>
+//                 <input type="text" className="form-control input input-bordered " placeholder="additional details" value="" />
+//             </div>
+
+//             <div className="text-center my-6"><button className="btn btn-primary profile-button" type="button">Save Profile</button></div>
+
+//         </div>
+
+//     );
+// };
+
+// export default Profile;
+
+
 import React from 'react';
 
 const Profile = () => {
     return (
-        <div className=''>
+        <div>
+            <h2 className=' text-center font-bold text-3xl my-10'>Profile Setting</h2>
 
-            <div className=" columns-4 flex justify-end ">
-                <img className="rounded-circle" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" alt='' />
-                <span className=" font-bold">Email : </span><span className="text-black-50">banglibabu@gmail.com</span>
-            </div>
+            <form>
+                <div class="grid md:grid-cols-2 md:gap-6">
+                    <div class="relative z-0 w-full mb-6 group">
+                        <input type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
+                    </div>
+                    <div class="relative z-0 w-full mb-6 group">
+                        <input type="text" name="floating_last_name" id="floating_last_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <label for="floating_last_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
+                    </div>
+                </div>
 
-            <h2 className='text-center font-bold text-4xl my-10'>Profile Settings</h2>
+                <div class="grid md:grid-cols-2 md:gap-6">
+                    <div class="relative z-0 w-full mb-6 group">
+                        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (123-456-7890)</label>
+                    </div>
+                    <div class="relative z-0 w-full mb-6 group">
+                        <input type="text" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
+                    </div>
+                </div>
 
-            <div className=' columns-8 mx-auto my-6'>
-                <label className="labels">Name</label>
-                <input type="text" className="form-control" placeholder="Full Name" value="" />
-                <label className="labels">Surname</label>
-                <input type="text" className="form-control" value="" placeholder="surname" />
-                <label className="labels">Phone/Mobile</label>
-                <input type="text" className="form-control" placeholder="enter phone/mobile" value="" />
-            </div>
+                <div class="grid md:grid-cols-2 md:gap-6">
+                    <div class="relative z-0 w-full mb-6 group">
+                        <input type="text" name="floating_present_adress" id="floating_present_adress" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <label for="floating_present_adress" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Present Address</label>
+                    </div>
+                    <div class="relative z-0 w-full mb-6 group">
+                        <input type="text" name="floating_permanent_adress" id="floating_permanent_adress" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <label for="floating_permanent_adress" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Permanent Address</label>
+                    </div>
 
-            <div className=" columns-8 mx-auto my-6">
-                <label className="labels">Address Line 1</label>
-                <input type="text" className="form-control" placeholder="enter address line 1" value="" />
-                <label className="labels">Address Line 2</label>
-                <input type="text" className="form-control" placeholder="enter address line 2" value="" />
-                <label className="labels">Postcode</label>
-                <input type="text" className="form-control" placeholder="enter address line 2" value="" />
-            </div>
+                </div>
 
-            <div className=" columns-8 mx-auto my-6">
-                <label className="labels">State</label>
-                <input type="text" className="form-control" placeholder="enter address line 2" value="" />
-                <label className="labels">Area</label>
-                <input type="text" className="form-control" placeholder="enter address line 2" value="" />
-                <label className="labels">Country</label>
-                <input type="text" className="form-control" placeholder="country" value="" />
-            </div>
-
-
-            <div className=" columns-8 my-6">
-                <label className="labels">Education</label>
-                <input type="text" className="form-control" placeholder="education" value="" />
-            </div>
+                <div class="grid  w-full md:gap-6">
+                    <div class="relative z-0 w-full mb-6 group">
+                        <input type="text" name="floating_present_experience" id="floating_present_experience" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <label for="floating_present_experience" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Experience</label>
+                    </div>
 
 
-            <div className="columns-8">
-                <label>Edit Experience</label>
-                <input type="text" className="form-control" placeholder="experience" value="" />
-            </div>
+                </div>
+                <div class="grid  w-full md:gap-6">
 
-            <div className=" columns-8 my-6">
-                <label className="labels">Additional Details</label>
-                <input type="text" className="form-control" placeholder="additional details" value="" />
-            </div>
+                    <div class="relative z-0 w-full mb-6 group">
+                        <input type="text" name="education_qualification" id="education_qualification" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <label for="education_qualification" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Education Qualification</label>
+                    </div>
 
-            <div className="text-center my-6"><button className="btn btn-primary profile-button" type="button">Save Profile</button></div>
+                </div>
+
+
+                <button type="submit" class=" left-auto my-10 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save Profile</button>
+            </form>
 
         </div>
-
     );
 };
 
