ee0578a19b2716e3fea9288097787be2e23ad249
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2023-01-10 11:55:34 +0600

    updated profile

--- src/Pages/Profile/CarrerAndApplication.js
+++ src/Pages/Profile/CarrerAndApplication.js
@@ -1,47 +1,27 @@
 import React from 'react';
 
-const AddressDetails = () => {
+const CarrerAndApplication = () => {
     return (
         <div>
             <div className=' mx-52  mt-10'>
                 <h2 className="collapse-title text-xl font-medium">
-                    Address Details
+                    Carrer and Application Information
                 </h2>
                 <div class="grid md:grid-cols-2 md:gap-6">
                     <div>
                         <label className="label">
                             <span className="label-text text-md font-bold ">Present Address</span>
                         </label>
-                        <input type="radio" name="inside-bangladesh" className="radio radio-primary" /> <label htmlFor="">Inside Bangladesh</label>
                         <input type="radio" name="outside-bangladesh" className="radio radio-primary" /> <label htmlFor="">Outside Bangladesh</label>
                     </div>
                 </div>
 
                 <div class="grid md:grid-cols-3 md:gap-6 mt-2">
+
                     <div>
-                        <select className="select select-bordered w-60 ">
-                            <option disabled selected>Select District</option>
-                            <option value={0}>Male</option>
-                            <option value={1}>Female</option>
-                            <option value={2}>Others</option>
-                        </select>
+
                     </div>
-                    <div>
-                        <select className="select select-bordered w-60 ">
-                            <option disabled selected>Select Thana</option>
-                            <option value={0}>Male</option>
-                            <option value={1}>Female</option>
-                            <option value={2}>Others</option>
-                        </select>
-                    </div>
-                    <div>
-                        <select className="select select-bordered w-60 ">
-                            <option disabled selected>Select P.O.</option>
-                            <option value={0}>Male</option>
-                            <option value={1}>Female</option>
-                            <option value={2}>Others</option>
-                        </select>
-                    </div>
+
                 </div>
                 <input className='input input-bordered w-full my-5 ' type="text" placeholder='Type your house no/road/village' />
 
@@ -88,4 +68,4 @@ const AddressDetails = () => {
     );
 };
 
-export default AddressDetails;
+export default CarrerAndApplication;
--- src/Pages/Profile/Profiles.js
+++ src/Pages/Profile/Profiles.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PersonalDetails from './PersonalDetails';
 import AddressDetails from './AddressDetails';
+import CarrerAndApplication from './CarrerAndApplication';
 
 const Profile = () => {
     return (
         <div>
             {/* <PersonalDetails></PersonalDetails> */}
-            <AddressDetails></AddressDetails>
+            {/* <AddressDetails></AddressDetails> */}
+            <CarrerAndApplication></CarrerAndApplication>
         </div>
     );
 };
