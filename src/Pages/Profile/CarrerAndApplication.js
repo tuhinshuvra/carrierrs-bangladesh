cafdb14709830cf2f5a432ab5b11e1cbecb5c225
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2023-01-11 11:19:11 +0600

    updated carrer and application form

--- src/Pages/Profile/CarrerAndApplication.js
+++ src/Pages/Profile/CarrerAndApplication.js
@@ -28,21 +28,38 @@ const CarrerAndApplication = () => {
                 <div class="grid md:grid-cols-2 md:gap-6">
                     <div>
                         <label className="label"> <span className="label-text text-md font-bold ">Looking for (Job Level)</span> </label>
-                        <div className=' flex  '>
-
+                        <div className=' grid grid-cols-3'>
                             <div className=' flex items-center'>
                                 <label for="html">Entry Level</label><input className=' ml-1' type="radio" id="entry-level" name="entry-level" value="entry-level" />
                             </div>
-                            <div className=' mx-5  flex items-center'>
-                                <label for="html">Mid Level</label><input className=' ml-1' type="radio" id="entry-level" name="entry-level" value="entry-level" />
+                            <div className=' mx-5 flex items-center'>
+                                <label for="html">Mid Level</label><input className=' ml-1' type="radio" id="mid-level" name="mid-level" value="mid-level" />
                             </div>
                             <div className=' flex items-center'>
-                                <label for="html">Top Level</label><input className=' ml-1' type="radio" id="entry-level" name="entry-level" value="entry-level" />
+                                <label for="html">Top Level</label><input className=' ml-1' type="radio" id="top-level" name="top-level" value="top-level" />
                             </div>
                         </div>
                     </div>
                     <div>
                         <label className="label"> <span className="label-text text-md font-bold ">Available for (Job Nature)</span></label>
+                        <div className=' grid grid-cols-4 '>
+                            <div className=' flex items-center'>
+                                <label for="html">Full time</label><input className=' ml-1' type="radio" id="full-time" name="full-time" value="full-time" />
+                            </div>
+                            <div className='flex items-center'>
+                                <label for="html">Part time</label><input className=' ml-1' type="radio" id="part-time" name="part-time" value="part-time" />
+                            </div>
+                            <div className=' flex items-center'>
+                                <label for="html">Contract</label><input className=' ml-1' type="radio" id="contact" name="contact" value="contact" />
+                            </div>
+                            <div className=' flex items-center'>
+                                <label for="html">Internship</label><input className=' ml-1' type="radio" id="internship" name="internship" value="internship" />
+                            </div>
+                            <div className=' flex items-center'>
+                                <label for="html">Freelance</label><input className=' ml-1' type="radio" id="freelance" name="freelance" value="freelance" />
+                            </div>
+                        </div>
+
                     </div>
                 </div>
 
