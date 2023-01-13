f38dc925b6da6b3f7880a779143bd34d284461c4
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2023-01-13 09:12:27 +0600

    added applied job list

--- src/Pages/Profile/MyJobs/AppliedJobList/AppliedJobList.js
+++ src/Pages/Profile/MyJobs/AppliedJobList/AppliedJobList.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const AppliedJobList = () => {
     return (
         <div>
-            <h2>Applied Job List</h2>
+            <h2 className=' text-center text-2xl font-bold my-10 '>Applied Job List</h2>
+            <ol className='max-w-md space-y-1 list-decimal list-inside '>
+                <Link> <li>Full Stack Web Developer</li></Link>
+                <Link> <li>Front End Web Developer</li></Link>
+                <Link> <li>Back End Web Developer</li></Link>
+                <Link> <li>Server Administrator</li></Link>
+                <Link> <li>Database Administrator</li></Link>
+            </ol>
         </div>
     );
 };
